c61ccd0dfe9f1de8fd32d2284f4d515c0455994f
no1harm <chen09869@gmail.com>
2019-01-09 17:43:29 +0800

    使用leanCloud存储歌曲信息

---
+++ src/js/initializers/av.js
@@ -0,0 +1,9 @@
+{
+    let APP_ID = '8hEQSYKwJEU7CanzAR8bEvrG-gzGzoHsz';
+    let APP_KEY = '58ACLXo4M9LF9e9sQKjrwg4Q';
+
+    AV.init({
+        appId: APP_ID,
+        appKey: APP_KEY
+    });
+}
--- src/js/song-form.js
+++ src/js/song-form.js
@@ -1,21 +1,26 @@
 {
     let view =  {
         el:'.form-container',
+        init(){
+            this.$el = $(this.el)
+        },
         template:`
         <form class="form">
             <div class="row">
                 <label>歌名</label>
-                <input type="text" value="__key__">
+                <input name="name" type="text" value="__key__">
             </div>
             <div class="row">
                 <label>歌手</label>
-                <input type="text" value="歌手">
+                <input name="singer" type="text" value="歌手">
             </div>
             <div class="row">
                 <label>外链</label>
-                <input type="text" value="__link__">
+                <input name="url" type="text" value="__link__">
             </div>
-            <input type="button" value="保存">
+            <div class="row">
+                <button type="submit" >保存</button>
+            </div>
         </form>
         `,
         render(data = {}){
@@ -27,16 +32,48 @@
             $(this.el).html(html)
         }
     }
-    let model = {}
+    let model = {
+        data:{
+            name:'',
+            singer:'',
+            url:''
+        },
+        saveSong(data){
+            let {name,singer,url} = data
+            let Song = AV.Object.extend('Song');
+            let song = new Song();
+            song.set('name',name);
+            song.set('singer',singer);
+            song.set('url',url);
+            song.save().then(function (newSong) {
+              console.log('newSong is ' + newSong);
+            }, function (error) {
+              console.error(error);
+            });
+        }
+    }
     let controller = {
         init(view,model){
             this.view = view
+            this.view.init()
             this.model = model
+            this.bindEvents()
             this.view.render(this.model.data)
             window.eventHub.on('upload',(data)=>{
                 console.log("songForm 接受到消息")
                 console.log(data)
                 this.reset(data)
+            })
+        },
+        bindEvents(){
+            this.view.$el.on('submit','form',(e)=>{
+                e.preventDefault();
+                let needs = ['name','singer','url']
+                let data = {}
+                needs.map((string)=>{
+                    data[string] = this.view.$el.find(`[name="${string}"]`).val()
+                })
+                this.model.saveSong(data)
             })
         },
         reset(data){
