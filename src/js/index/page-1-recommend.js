307650b84745a2655393f4a45ba0e812b2468b7a
no1harm <chen09869@gmail.com>
2019-01-13 20:02:00 +0800

    通过创建script标签加载模块

---
+++ src/js/index/page-1-recommend.js
@@ -0,0 +1,11 @@
+{
+    let view = {}
+    let model = {}
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+        }
+    }
+    controller.init(view,model)
+}
--- src/js/index/page-1.js
+++ src/js/index/page-1.js
@@ -18,6 +18,8 @@
             this.view.init()
             this.model = model
             this.bindEventsHub()
+            this.loadModule1()
+            this.loadModule2()
         },
         bindEventsHub(){
             window.eventHub.on('selectedTab',(data)=>{
@@ -27,6 +29,22 @@
                     this.view.hide()
                 }
             })
+        },
+        loadModule1(){
+            let script = document.createElement('script')
+            script.src = "./js/index/page-1-recommend.js"
+            script.onload = function(){
+                console.log('1 is loading')
+            }
+            document.body.appendChild(script)
+        },
+        loadModule2(){
+            let script = document.createElement('script')
+            script.src = "./js/index/page-1-new.js"
+            script.onload = function(){
+                console.log('2 is loading')
+            }
+            document.body.appendChild(script)
         }
     }
     controller.init(view,model)
