6fdb59e64a256c72a5131b9c5d72ac9f51fedc50
no1harm <chen09869@gmail.com>
2019-01-13 20:21:30 +0800

    完善最新歌曲的MVC

--- src/js/index/page-1-new.js
+++ src/js/index/page-1-new.js
@@ -1,10 +1,61 @@
 {
-    let view = {}
-    let model = {}
+    let view = {
+        el:"section.songs",
+        init(){
+            this.$el = $(this.el)
+        },
+        render(data){
+            let {songs} = data
+            songs.map((song)=>{
+                let $li = $(`
+                <li>
+                <h3>${song.name}</h3>
+                <p>
+                    <svg class="icon icon-sq">
+                    <use xlink:href="#icon-sq"></use>
+                    </svg>
+                    ${song.singer}
+                </p>
+                <a class="playButton" href="#">
+                    <svg class="icon icon-play">
+                    <use xlink:href="#icon-play"></use>
+                    </svg>
+                </a>
+                </li>
+                `)
+                this.$el.find('ol.list').append($li)
+            })
+        }
+    }
+    let model = {
+        data:{
+            songs:[]
+        },
+        fetch(){
+            let query = new AV.Query('Song')
+            return query.find().then((songs)=>{
+                this.data.songs = songs.map((song)=>{
+                    return {id:song.id,...song.attributes}
+                })
+                return songs
+            })
+        }
+    }
     let controller = {
         init(view,model){
             this.view = view
+            this.view.init()
             this.model = model
+            this.bindEvent()
+            this.getSongs()
+        },
+        bindEvent(){
+
+        },
+        getSongs(){
+            this.model.fetch().then(()=>{
+                this.view.render(this.model.data)
+            })
         }
     }
     controller.init(view,model)
