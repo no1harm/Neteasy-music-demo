defe579de7062560b17444a7f403d142046b74f4
no1harm <chen09869@gmail.com>
2019-01-14 13:36:47 +0800

    通过id获取歌曲信息

--- src/js/song/app.js
+++ src/js/song/app.js
@@ -1,18 +1,54 @@
-let search = window.location.search
-
-search = search.substring(1)
-
-let arr = search.split('&').filter(v => v)
-
-let id = ''
-
-for(let i =0;i<arr.length;i++){
-    let kv = arr[i].split('=')
-    let key = kv[0]
-    let value = kv[1]
-    if(key === 'id'){
-        id = value
+{
+    let view =  {}
+    let model = {
+        data:{
+            name:'',
+            singer:'',
+            url:'',
+            id:''
+        },
+        setId(id){
+            this.data.id = id
+        },
+        getSong(id){
+            let query = new AV.Query('Song');
+            return query.get(id).then((song) => {
+                Object.assign(this.data,song.attributes)
+                return song
+            }, (error)=> {
+                console.log(error)
+            })
+        }
     }
-}
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            let id = this.getSongId()
+            this.model.setId(id)
+            this.model.getSong(id).then((data)=>{
+                console.log(this.model.data)
+            })
+        },
+        getSongId(){
+            let search = window.location.search
 
-console.log(id)
+            search = search.substring(1)
+
+            let arr = search.split('&').filter(v => v)
+
+            let id = ''
+
+            for(let i =0;i<arr.length;i++){
+                let kv = arr[i].split('=')
+                let key = kv[0]
+                let value = kv[1]
+                if(key === 'id'){
+                    id = value
+                }
+            }
+            return id
+        }
+    }
+    controller.init(view,model)
+}
