b1f47d78c056296c5b5f5c072c34002716abf064
no1harm <chen09869@gmail.com>
2019-01-17 17:35:53 +0800

    获取所选歌曲的id

--- src/js/editSongList/get-all-song.js
+++ src/js/editSongList/get-all-song.js
@@ -3,12 +3,26 @@
         el:'.all-songs-list',
         init(){
             this.$el = $(this.el)
+        },
+        render(data){
+            let {songs} = data
+            songs.map((song)=>{
+                let $div = `
+                <div>
+                    <input type="checkbox" id="${song.id}" name="selectedSong" value="${song.id}" data-song-id="${song.id}">
+                    <label for="${song.id}">${song.name}</label>
+                </div>
+              `
+                // let $li = `<li data-song-id="${song.id}">${song.name}</li>`
+                this.$el.find('form').append($div)
+            })
+            this.$el.find('form').append(`<button type="submit">提交</button>`)
         }
     }
     let model = {
         data:{
             songs:[],
-            selectedSongId:undefined
+            selectedSongs:[]
         },
         find(){
             let query = new AV.Query('Song')
@@ -25,8 +39,24 @@
             this.view = view
             this.view.init()
             this.model = model
-            this.model.find()
-            console.log(this.model.data)
+            this.model.find().then(()=>{
+                this.view.render(this.model.data)
+            })
+            this.bindEvents()
+        },
+        bindEvents(){
+            this.view.$el.on('submit','form',(e)=>{
+                e.preventDefault()
+                
+                let checkedSong = []
+                let $checkedSongList = $("input[name='selectedSong']:checked")
+                for(let i=0;i<$checkedSongList.length;i++){
+                    let id = $checkedSongList[i].getAttribute('data-song-id')
+                    checkedSong.push(id)
+                }
+                this.model.selectedSongs = checkedSong
+                console.log(this.model.selectedSongs)
+            })
         }
     }
     controller.init(view,model)
