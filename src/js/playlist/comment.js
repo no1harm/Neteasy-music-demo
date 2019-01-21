0b243d559b2b829ab3a626ab7fe5db20741ce490
no1harm <chen09869@gmail.com>
2019-01-21 15:44:29 +0800

    数据库添加评论

--- src/js/playlist/comment.js
+++ src/js/playlist/comment.js
@@ -3,11 +3,36 @@
         el:'section.comments',
         init(){
             this.$el = $(this.el)
+        },
+        render(data){
+            let $li = $(`
+            <li><li>
+            `)
         }
     }
     let model = {
         playListId:'',
-        fetch(data){}
+        comments:[],
+        currentComment:{},
+        fetch(data){},
+        setComment(data){
+            let playlist = AV.Object.createWithoutData('Playlist',this.playListId)
+            let Comments = AV.Object.extend('Comments')
+            let comment = new Comments()
+            comment.set('name','游客')
+            comment.set('content',data)
+            comment.set('dependent', playlist)
+            return comment.save().then((newComment) => {
+                let data = {}
+                let {id,attributes} = newComment
+                Object.assign(this.currentComment,{
+                    id,
+                    ...attributes
+                })
+            }, function (error) {
+                console.error(error)
+            })
+        }
     }
     let controller = {
         init(view,model){
@@ -21,7 +46,10 @@
         bindeEvents(){
             this.view.$el.on('click','#submit-comment',(e)=>{
                 let value = this.view.$el.find('.comment-input').text()
-                console.log(value)
+                this.model.setComment(value).then(()=>{
+                    console.log(this.model.currentComment)
+                    this.view.render(this.model.currentComment)
+                })
             })
         },
         bindEventsHub(){
