d80cbb552281fb70f129d8d1684d343cf3ae77f9
no1harm <chen09869@gmail.com>
2019-01-23 18:03:14 +0800

    后台歌手页面获取所有歌手信息

---
+++ src/js/admin-singer/singer-list.js
@@ -0,0 +1,33 @@
+{
+    let view = {
+        el:'.singer-list .singer-list',
+        init(){
+            this.$el = $(this.el)
+        }
+    }
+    let model = {
+        data:{
+            singers:[]
+        },
+        fetch(){
+            let query = new AV.Query('Singer')
+            return query.find().then((singers)=>{
+                this.data.singers = singers.map((singer)=>{
+                    return {id:singer.id,...singer.attributes}
+                })
+                return singers
+            })
+        }
+    }
+    let controller = {
+        init(view,model){
+            this.view = view
+            this.model = model
+            this.view.init()
+            this.model.fetch().then( data =>{
+                console.log(this.model.data)
+            })
+        }
+    }
+    controller.init(view,model)
+}
